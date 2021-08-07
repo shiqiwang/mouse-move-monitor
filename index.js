19ddf1b643cab2224d8f916572d0083c323ae61b
shiqiwang <shiqi.wang@live.com>
2021-08-07 17:45:07 +0800

    broadcast

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const robot = require('robotjs');
+const os = require('os');
 const dgram = require('dgram');
 
 const server = dgram.createSocket('udp4');
 const client = dgram.createSocket('udp4');
 
+
+// 获取本机ip地址
+let ip = 'localhost';
+try {
+  const interfaces = os.networkInterfaces();
+  for (let dev in interfaces) {
+    interfaces[dev].forEach((details, _alias) => {
+      if (details.family === 'IPv4' && details.address !== '127.0.0.1' && !details.internal) {
+        ip = details.address;
+      }
+    });
+  }
+} catch (error) {
+  console.log(`get ip address error: ${error}`);
+}
+
 client.on('message', (message, _info) => {
   console.log(message.toString())
 })
-server.bind(1210);
+server.bind(1210, () => {
+  server.setBroadcast(true);
+});
 client.bind(1212);
 
 
 let lastMousePosition = robot.getMousePos();
-let message = 'mousemove';
+let message = `${ip} mousemove`;
 const timer = setInterval(() => {
   const mousePosition = robot.getMousePos();
   if(lastMousePosition.x !== mousePosition.x || lastMousePosition.y !== mousePosition.y) {
     lastMousePosition = mousePosition;
-    server.send(message, 0, message.length, 1212, 'localhost');
+    server.send(message, 0, message.length, 1212, '255.255.255.255');
   } else {
     return;
   }
